95c5d8b2f48c9724ea5ad51710369145d80026e0
Grant Eaton <Grantman100@gmail.com>
2016-10-29 18:44:09 -0400

    added scoredbreakthroughMove

--- TeamA01BreakthroughPlayer.java
+++ TeamA01BreakthroughPlayer.java
@@ -1,6 +1,8 @@
 package breakthrough;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 import game.GameMove;
 import game.GamePlayer;
@@ -9,10 +11,33 @@ import game.Util;
 
 public class TeamA01BreakthroughPlayer extends GamePlayer {
 
-	public TeamA01BreakthroughPlayer(String n) 
+	public final int MAX_DEPTH = 50;
+	public int depthLimit;
+	
+	protected class scoredBreakthroughMove extends BreakthroughMove{
+		public double score;
+		public scoredBreakthroughMove(int r1, int c1, int r2, int c2, double s) {
+			super(r1,c1,r2,c2);
+			score = s;
+		}
+
+		public void set(int r1, int c1, int r2, int c2, double s) {
+			startRow = r1;
+			startCol = c1;
+			endingRow = r2;
+			endingCol = c2;
+			score = s;
+		}
+
+	}
+	
+	
+	public TeamA01BreakthroughPlayer(String n, int d) 
 	{
 		super(n, "Breakthrough");
+		depthLimit = d;
 	}
+	
 	public void messageFromOpponent(String m)
 	{ System.out.println(m); }
 	private boolean validSquare(BreakthroughState brd, int row, int col)
@@ -49,68 +74,26 @@ public class TeamA01BreakthroughPlayer extends GamePlayer {
 		
 		return safe;
 	}
-	public GameMove getMove(GameState state, String lastMove)
+	
+	
+	public GameMove getMove(GameState state, string lastMove)
 	{
 		BreakthroughState board = (BreakthroughState)state;
-		ArrayList<BreakthroughMove> takes = new ArrayList<BreakthroughMove>();  
-		ArrayList<BreakthroughMove> safeMoves = new ArrayList<BreakthroughMove>();  
-		ArrayList<BreakthroughMove> vulnerableMoves = new ArrayList<BreakthroughMove>();  
-		BreakthroughMove mv = new BreakthroughMove();
-		char me = state.who == GameState.Who.HOME ?
-				BreakthroughState.homeSym : BreakthroughState.awaySym;
-		char opp = state.who == GameState.Who.HOME ?
-				BreakthroughState.awaySym : BreakthroughState.homeSym;
-		int dir = state.who == GameState.Who.HOME ? +1 : -1;
-		for (int r=0; r<BreakthroughState.N; r++) {
-			for (int c=0; c<BreakthroughState.N; c++) {
-				mv.startRow = r;
-				mv.startCol = c;
-				mv.endingRow = r+dir; mv.endingCol = c;
-				if (board.moveOK(mv)) {
-					if (board.board[mv.endingRow][mv.endingCol] == opp) {
-						takes.add((BreakthroughMove)mv.clone());
-					} else if (safeMove(board, mv, dir, me, opp)){
-						safeMoves.add((BreakthroughMove)mv.clone());
-					} else {
-						vulnerableMoves.add((BreakthroughMove)mv.clone());						
-					}
-				}
-				mv.endingRow = r+dir; mv.endingCol = c+1;
-				if (board.moveOK(mv)) {
-					if (board.board[mv.endingRow][mv.endingCol] == opp) {
-						takes.add((BreakthroughMove)mv.clone());
-					} else if (safeMove(board, mv, dir, me, opp)){
-						safeMoves.add((BreakthroughMove)mv.clone());
-					} else {
-						vulnerableMoves.add((BreakthroughMove)mv.clone());						
-					}
-				}
-				mv.endingRow = r+dir; mv.endingCol = c-1;
-				if (board.moveOK(mv)) {
-					if (board.board[mv.endingRow][mv.endingCol] == opp) {
-						takes.add((BreakthroughMove)mv.clone());
-					} else if (safeMove(board, mv, dir, me, opp)){
-						safeMoves.add((BreakthroughMove)mv.clone());
-					} else {
-						vulnerableMoves.add((BreakthroughMove)mv.clone());						
-					}
-				}
-			}
-		}
-		ArrayList<BreakthroughMove> list;  
-		if (takes.size() > 0) {
-			list = takes;
-		} else if (safeMoves.size() > 0){
-			list = safeMoves;
-		} else {
-			list = vulnerableMoves;
-		}
-		int which = Util.randInt(0, list.size()-1);
-		return list.get(which);
+		
+		Comparator<>
+		PriorityQueue<>
+		return 
+		
+		
 	}
 	public static void main(String [] args)
 	{
-		GamePlayer p = new TeamA01BreakthroughPlayer("Greedy BT+");
+		GamePlayer p = new TeamA01BreakthroughPlayer("team A01 BT+");
 		p.compete(args);
+	}
+	@Override
+	public GameMove getMove(GameState state, String lastMv) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
